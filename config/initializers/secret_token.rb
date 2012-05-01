04049e51494a7f2b2afe192950d24042a1dff66b
Simao Belchior de Castro <simaobelchior@gmail.com>
2012-05-01 14:15:40 +0100

    created project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CitesChecklist::Application.config.secret_token = '3122d313a3386b537642a1d4149cc877fe78fc8dbdde8f574178b5d79cc7910a891d15dee5176c3fe87170e20fffd4d22766cf76ebb23558c68331585d9d3acc'
