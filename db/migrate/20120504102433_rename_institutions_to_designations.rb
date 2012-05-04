9edf4287d9e56e7d27f551389636ed4fd2ef096a
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2012-05-04 14:05:13 +0100

    removed problematic statments from migration

--- db/migrate/20120504102433_rename_institutions_to_designations.rb
+++ db/migrate/20120504102433_rename_institutions_to_designations.rb
@@ -1,8 +1,5 @@
 class RenameInstitutionsToDesignations < ActiveRecord::Migration
   def up
-    Institution.delete_all
-    TaxonRelationship.delete_all
-    Taxon.delete_all
     drop_table :taxons_institutions
     rename_table :institutions, :designations
     add_column :taxa, :designation_id, :integer, :null => false
