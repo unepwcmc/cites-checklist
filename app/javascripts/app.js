19294f13f218543d712fc16cbe2a6cb68fa6bbcb
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2012-05-18 10:49:40 +0100

    added nested taxon concepts to the list

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -34,30 +34,45 @@ Checklist.store = DS.Store.create({
 
 Checklist.TaxonConcept = DS.Model.extend({
   taxon_name: DS.belongsTo('Checklist.TaxonName', { embedded: true }),
+  depth: DS.attr('number'),
   created_at: DS.attr('date'),
   full_name: Ember.computed(function() {
-    return this.get('created_at') + ' ' + this.get('taxon_name_id');
-  }).property('created_at', 'taxon_name_id')
+    return this.getPath('taxon_name.scientific_name');
+  }).property('taxon_name.scientific_name', 'taxon_name_id')
 
 });
 
 Checklist.TaxonConcept.reopenClass({
-  url: 'checklists'
+  url: 'taxon_concepts'
 });
 
 Checklist.TaxonName = DS.Model.extend({
   scientific_name: DS.attr('string')
 });
 
+Checklist.TaxonTree = DS.Model.extend({
+  taxon_concepts: DS.hasMany('Checklist.TaxonConcept', { embedded: true})
+});
+
+Checklist.TaxonTree.reopenClass({
+  url: 'taxon_trees'
+});
+
+Checklist.TaxonTreeController = Ember.ArrayController.create({
+  content: Checklist.store.findAll(Checklist.TaxonTree),
+});
+
+/*
 Checklist.TaxonConceptController = Ember.ArrayController.create({
   content: Checklist.store.findAll(Checklist.TaxonConcept),
 });
-
+*/
 Checklist.MainView = Ember.View.extend({
   templateName: 'main_view',
-  taxonConceptsBinding: 'Checklist.TaxonConceptController'
+  //taxonConceptsBinding: 'Checklist.TaxonConceptController'
+  taxonTreesBinding: 'Checklist.TaxonTreeController'
 });
 
-Checklist.TaxonConceptShowView = Ember.View.extend({
+Checklist.TaxonTreeShowView = Ember.View.extend({
   templateName: 'show_view'
 })
