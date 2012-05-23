47286b5298e429bfb01cf76e7e4bf4947e375e59
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2012-05-23 08:49:57 +0100

    added XDomainRequest for IE 8

--- app/javascripts/sapi_adapter.js
+++ app/javascripts/sapi_adapter.js
@@ -1,29 +1,20 @@
+//TODO refactor
 Checklist.SAPIAdapter = DS.Adapter.extend({
+
   findAll: function(store, type) {
     var url = this.url + type.collectionUrl;
-/*
-    jQuery.getJSON(url, {format: 'json', jsoncallback: '?'}, function(data) {
-      // data is an Array of Hashes in the same order as the original
-      // Array of IDs. If your server returns a root, simply do something
-      // like:
-      // store.loadMany(type, ids, data.people)
-      store.loadMany(type, data);
-    });
-*/
 
     if($.browser.msie && window.XDomainRequest) {
-      alert('hello');
       // Use Microsoft XDR
       var xdr = new XDomainRequest();
+      xdr.contentType = "text/plain";
       xdr.open("get", url);
-      xdr.onerror = function(){
-        alert('error');
-      }
+      xdr.onprogress = function(){};
+      xdr.onerror = function(){};
       xdr.onload = function () {
         var JSON = $.parseJSON(xdr.responseText);
-        if (JSON == null || typeof (JSON) == 'undefined')
-        {
-            JSON = $.parseJSON(data.firstChild.textContent);
+        if (JSON == null || typeof (JSON) == 'undefined'){
+          JSON = $.parseJSON(data.firstChild.textContent);
         }
         store.loadMany(type, JSON);
       };
@@ -33,32 +24,44 @@ Checklist.SAPIAdapter = DS.Adapter.extend({
         type : "GET",
         url : url,
         dataType : "json",
-        data : {
-          format : 'json',
-          jsoncallback : '?'
-        },
-        //cache: false, // don't cache the result
-        //contentType: "application/json", //tell the server we're looking for json
+        data : {format : 'json', jsoncallback : '?'},
         success : function(data) {
           store.loadMany(type, data);
         },
-        error : function(xhr, status, error) {
-          alert(xhr.status);
-        }
+        error : function(xhr, status, error) {}
       });
     }
 
   },
   findQuery: function(store, type, query, modelArray) {
     var url = this.url + type.collectionUrl;
-    jQuery.getJSON(url, $.extend(query, {format: 'json', jsoncallback: '?'}), function(data) {
-        // data is expected to be an Array of Hashes, in an order
-        // determined by the server. This order may be specified in
-        // the query, and will be reflected in the view.
-        //
-        // If your server returns a root, simply do something like:
-        // modelArray.load(data.people)
-        modelArray.load(data);
-    });
+
+    if($.browser.msie && window.XDomainRequest) {
+      // Use Microsoft XDR
+      var xdr = new XDomainRequest();
+      xdr.contentType = "text/plain";
+      xdr.open("get", url);
+      xdr.onprogress = function(){};
+      xdr.onerror = function(){};
+      xdr.onload = function () {
+        var JSON = $.parseJSON(xdr.responseText);
+        if (JSON == null || typeof (JSON) == 'undefined'){
+          JSON = $.parseJSON(data.firstChild.textContent);
+        }
+        modelArray.load(JSON);
+      };
+      xdr.send(query);
+    } else {
+      $.ajax({
+        type : "GET",
+        url : url,
+        dataType : "json",
+        data : $.extend(query, {format: 'json', jsoncallback: '?'}),
+        success : function(data) {
+          modelArray.load(data);
+        },
+        error : function(xhr, status, error) {}
+      });
+    }
   }
 });
