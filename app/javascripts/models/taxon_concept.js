19a6ddc162f884fc0e0e23f26ab6a9499f80a08e
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2012-05-24 10:45:15 +0100

    simplified name display

--- app/javascripts/models/taxon_concept.js
+++ app/javascripts/models/taxon_concept.js
@@ -1,10 +1,8 @@
 Checklist.TaxonConcept = DS.Model.extend({
   taxon_name: DS.belongsTo('Checklist.TaxonName', { embedded: true }),
   depth: DS.attr('number'),
-  created_at: DS.attr('date'),
-  full_name: Ember.computed(function() {
-    return this.getPath('taxon_name.scientific_name');
-  }).property('taxon_name.scientific_name', 'taxon_name_id')
+  scientific_name: DS.attr('string'),
+  rank_name: DS.attr('string')
 });
 
 Checklist.TaxonConcept.reopenClass({
