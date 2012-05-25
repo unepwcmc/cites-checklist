3ca046561c40ee7993f711e82702c6aed08c7580
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2012-05-25 11:52:07 +0100

    added binomial and trinomial names

--- app/javascripts/models/taxon_concept.js
+++ app/javascripts/models/taxon_concept.js
@@ -1,8 +1,28 @@
 Checklist.TaxonConcept = DS.Model.extend({
   taxon_name: DS.belongsTo('Checklist.TaxonName', { embedded: true }),
+  parent: DS.belongsTo('Checklist.TaxonConcept', { key: 'parent_id'}),
   depth: DS.attr('number'),
   scientific_name: DS.attr('string'),
-  rank_name: DS.attr('string')
+  rank_name: DS.attr('string'),
+  binomen: function() {
+    return this.getPath('parent.scientific_name') +
+           " " + this.get('scientific_name');
+  }.property('parent.scientific_name', 'scientific_name'),
+  trinomen: function() {
+    return this.getPath('parent.parent.scientific_name') +
+           " " + this.getPath('parent.scientific_name') +
+           " ssp. " + this.get('scientific_name');
+  }.property('parent.scientific_name', 'scientific_name'),
+  full_name: function() {
+    console.log(this.get('rank_name'));
+    if (this.get('rank_name') == 'Species') {
+      return this.get('binomen');
+    } else if (this.get('rank_name') === 'Subspecies') {
+      return this.get('trinomen');
+    } else {
+      return this.get('rank_name') + ': ' + this.get('scientific_name');
+    }
+  }.property('rank_name','scientific_name'),
 });
 
 Checklist.TaxonConcept.reopenClass({
