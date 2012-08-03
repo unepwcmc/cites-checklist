086b12ae48a3bec673ae11bbd08bb757f201c7e5
Adam Mulligan <adammulligan@gmail.com>
2012-08-03 11:07:56 +0100

    Added the ability to delete saved searches

--- app/javascripts/controllers/saved_search_controller.js
+++ app/javascripts/controllers/saved_search_controller.js
@@ -1,18 +1,3 @@
 Checklist.SavedSearchController = Ember.ArrayController.extend({
   content: Checklist.local_store.findAll(Checklist.SavedSearch),
-
-  selectionDidChange: function() {
-    // The first item of the searches drop down is empty and this method
-    // should return straight away if that's selected
-    // Remove this line if we move away from using a drop down
-    if (this.get('selection') == null) return;
-
-    var selection = this.get('selection').get('id');
-    var record    = Checklist.local_store.find(Checklist.SavedSearch, selection);
-    var filters   = JSON.parse(record.get('filters'));
-
-    var params = $.param(filters);
-
-    Checklist.get('router').transitionTo('search',{params: params});
-  }.observes('selection')
 });
---
+++ app/javascripts/views/saved_search_collection_view.js
@@ -0,0 +1,24 @@
+Checklist.SavedSearchCollectionView = Ember.CollectionView.extend({
+  tagName: 'ul',
+  content: [],
+  itemViewClass: Ember.View.extend({
+    contextBinding: 'content',
+    templateName: 'saved_search_collection',
+    load: function(event) {
+      var selection = this.get('content').get('id');
+      var record    = Checklist.local_store.find(Checklist.SavedSearch, selection);
+      var filters   = JSON.parse(record.get('filters'));
+
+      var params = $.param(filters);
+
+      Checklist.get('router').transitionTo('search',{params: params});
+    },
+    delete: function(event) {
+      var id     = this.get('content').get('id');
+      var search = Checklist.local_store.find(Checklist.SavedSearch, id);
+
+      search.deleteRecord();
+      Checklist.local_store.commit();
+    }
+  }),
+});
