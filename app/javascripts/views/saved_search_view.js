5bef4d8979b2372d0ed67a1cb505b88c649fd423
Adam Mulligan <adammulligan@gmail.com>
2012-08-15 15:54:08 +0100

    Removed the ability to save searches without names (meaning you can't select them from the list)

--- app/javascripts/views/saved_search_view.js
+++ app/javascripts/views/saved_search_view.js
@@ -3,11 +3,14 @@ Checklist.SavedSearchView = Em.View.extend({
   filtersController: null,
 
   save: function(view) {
+    var val = $(view.target).siblings('input').val();
+    if (!val) return;
+
     var saved_search = Checklist.local_store.createRecord(
       Checklist.SavedSearch,
       {
         id: Checklist.Helpers.generateId(),
-        name: $(view.target).siblings('input').val(),
+        name: val,
         filters: JSON.stringify(this.get('filtersController').toParams()),
       }
     );
