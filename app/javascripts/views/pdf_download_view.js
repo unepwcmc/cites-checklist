001216f0e66c13e84d961f87de2df7ec59213521
Adam Mulligan <adammulligan@gmail.com>
2012-08-15 13:56:58 +0100

    Fixed "full" pdf downloads by abstracting the download methods in to a single method that takes parameters

--- app/javascripts/views/pdf_download_view.js
+++ app/javascripts/views/pdf_download_view.js
@@ -2,32 +2,49 @@ Checklist.PdfDownloadView = Em.View.extend({
   templateName: 'pdf_download_view',
   filtersController: null,
 
-  indexPdfLink: function(){
-    return Checklist.store.adapter.url +
-    Checklist.Index.urlFromParams(
-      $.extend(
-        this.get('filtersController').toParams(),
-        {'format' : 'pdf'}
-      )
-    );
-  }.property().volatile(),
+  /*
+   * Returns the URL for the desired PDF.
+   *
+   * @params [Object] an object with the following keys:
+   *    { type: <Checklist Model>,
+   *      custom: <Boolean>(Use current params or not) }
+   */
+  pdfLink: function(options){
+    if (typeof options !== "object") {
+      options = { type: "Index",
+                  custom: false };
+    }
 
-  historyPdfLink: function(){
+    params = this.get('filtersController').toParams();
+
+    // For non-custom PDFs, maintain only the selected output layout
+    if (!options.custom) {
+      params = {output_layout: params.output_layout}
+    }
+
     return Checklist.store.adapter.url +
-    Checklist.History.urlFromParams(
+    Checklist[options.type].urlFromParams(
       $.extend(
-        this.get('filtersController').toParams(),
+        params,
         {'format' : 'pdf'}
       )
     );
-  }.property().volatile(),
+  },
+
+  downloadIndexPdf: function(event) {
+    window.location = this.pdfLink({type: 'Index', custom: false});
+  },
+
+  downloadHistoryPdf: function(event) {
+    window.location = this.pdfLink({type: 'History', custom: false});
+  },
 
   downloadCustomIndexPdf: function(event) {
-    window.location = this.get('indexPdfLink')
+    window.location = this.pdfLink({type: 'Index', custom: true});
   },
 
   downloadCustomHistoryPdf: function(event) {
-    window.location = this.get('historyPdfLink');
+    window.location = this.pdfLink({type: 'History', custom: true});
   }
 
 });
