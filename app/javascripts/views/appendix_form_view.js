fb7bb78c705e9b75ef3c800a949da977bf1f3589
Adam Mulligan <adammulligan@gmail.com>
2012-09-04 09:18:20 +0100

    Linked new appendix filter with filtersController

--- app/javascripts/views/appendix_form_view.js
+++ app/javascripts/views/appendix_form_view.js
@@ -1,26 +1,47 @@
-Handlebars.registerHelper('appendixColour', function(property, options) {
-  var value = Ember.Handlebars.getPath(this, property, options);
-
-  switch(value) {
-    case "I":
-      return "blue";
-    case "II":
-      return "green";
-    case "III":
-      return "orange";
-  }
-
-  return "";
-});
-
 Checklist.AppendixFormView = Ember.CollectionView.extend({
   tagName: 'ul',
   content: [],
   itemViewClass: Ember.View.extend({
     contextBinding: 'content',
     templateName: 'appendix_form',
+
+    classNameBindings: ['classes'],
+    classes: function() {
+      var filtersController = Checklist.get('router').get('filtersController');
+      var abbreviation      = this.get('context').get('abbreviation');
+
+      var classes = [];
+
+      switch(abbreviation) {
+        case "I":
+          classes.push("blue");
+          break;
+        case "II":
+          classes.push("green");
+          break;
+        case "III":
+          classes.push("orange");
+          break;
+      }
+
+      if ($.inArray(abbreviation, filtersController.get('appendices').mapProperty('abbreviation')) < 0) {
+        classes.push("inactive");
+      }
+
+      return classes.join(" ");
+    }.property().volatile(),
+
     click: function(event) {
-      $(event.target).toggleClass('inactive');
+      var filtersController = Checklist.get('router').get('filtersController');
+
+      // Add the selected appendice to the appendice filter array
+      // Equivalent to a selectionBinding in a dropdown list
+      appendices = filtersController.get('appendices');
+      if (!appendices.contains(this.get('context'))) {
+        appendices.push(this.get('context'))
+      }
+
+      $(event.target).parent().toggleClass('inactive');
     }
   }),
 });
