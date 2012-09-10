249546787cc71ae4de2a2e5f6091429ba44d254d
Adam Mulligan <adammulligan@gmail.com>
2012-09-10 15:36:32 +0100

    Rank styling dependence for list items

--- app/javascripts/views/taxon_concept_list_view.js
+++ app/javascripts/views/taxon_concept_list_view.js
@@ -2,14 +2,17 @@ Checklist.TaxonConceptListView = Ember.CollectionView.extend({
   content: null,
   filtersController: null,
   itemViewClass: Ember.View.extend({
+    tagName: 'div',
     contextBinding: 'content',
     templateName: 'taxon_concept_view',
-    classNames: ['listing-item'],
-    classNameBindings: ['rank'],
+
     //TODO could the filters controller be passed here and used instead of router call
     filtersController: function(){
-      return Checklist.get('router').get('filtersController')
+      return Checklist.get('router').get('filtersController');
     }.property(),
+
+    classNames: ['listing-item'],
+    classNameBindings: ['rank'],
     rank: function(){
       var classes = [];
 
@@ -25,34 +28,66 @@ Checklist.TaxonConceptListView = Ember.CollectionView.extend({
 
       return classes.join(" ");
     }.property(),
+
+    headingStyle: function() {
+      var style = "";
+
+      if (this.content.get('cites_accepted')) {
+        style += "font-weight:normal;";
+      }
+
+      switch(this.content.get('rank_name')) {
+        case "FAMILY":
+        case "ORDER":
+          style += "text-transform:uppercase;font-style:normal;";
+          break;
+      }
+
+      return style;
+    }.property(),
+    full_name: function() {
+      var full_name = this.content.get('full_name');
+
+      switch (this.content.get('rank_name')) {
+        case "FAMILY":
+        case "GENUS":
+          full_name += " spp.";
+          break;
+      }
+
+      return full_name;
+    }.property('full_name'),
+
     showEnglish: function(){
       return this.get('filtersController').showEnglish &&
-        this.content.get('english').length > 0
+        this.content.get('english').length > 0;
     }.property(),
     showSpanish: function(){
       return this.get('filtersController').showSpanish &&
-        this.content.get('spanish').length > 0
+        this.content.get('spanish').length > 0;
     }.property(),
     showFrench: function(){
       return this.get('filtersController').showFrench &&
-        this.content.get('french').length > 0
+        this.content.get('french').length > 0;
     }.property(),
     showSynonyms: function(){
       return this.get('filtersController').showSynonyms &&
-        this.content.get('synonyms').length > 0
+        this.content.get('synonyms').length > 0;
     }.property(),
     showListing: function(){return this.content.get('rank_name') != 'KINGDOM' &&
       this.content.get('rank_name') != 'PHYLUM' &&
-      this.content.get('rank_name') != 'CLASS'
+      this.content.get('rank_name') != 'CLASS';
     }.property(),
+
     isHigherTaxa: function(){
       return this.content.get('rank_name') == 'higher-taxa';
     }.property(),
     showHigherTaxaBar: function(){
       return this.get('filtersController').taxonomicLayout &&
         this.get('isHigherTaxa');
+
     }.property(),
-    tagName: 'div',
+
     designationColour: function() {
       var appendix = this.get('context').get('current_listing');
       switch (appendix) {
@@ -65,6 +100,9 @@ Checklist.TaxonConceptListView = Ember.CollectionView.extend({
       }
     }.property(),
 
+    /*
+     * Handle hover and click states of each species in the listing
+     */
     mouseEnter: function(event) {
       if (!this.$().hasClass('expanded')) {
         this.$().children('.opener-holder').fadeIn();
@@ -80,5 +118,5 @@ Checklist.TaxonConceptListView = Ember.CollectionView.extend({
       this.$().toggleClass('expanded');
       this.$().find('.slide').slideToggle();
     }
-  }),
+  })
 });
