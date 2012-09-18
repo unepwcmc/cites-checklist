d1e5fcb5f46435a16dc2e753c524d64dd8936985
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2012-09-18 15:44:45 +0100

    added raw version of info popups

--- app/javascripts/views/timeline_event_list_view.js
+++ app/javascripts/views/timeline_event_list_view.js
@@ -7,6 +7,9 @@ Checklist.TimelineEventListView = Ember.CollectionView.extend({
     colour: function(){
       return this.get('parentView.parentView.colour');
     }.property(),
+    appendix: function(){
+      return this.get('parentView.parentView.content.appendix');
+    }.property(),
     tooltip: function(){
       var res = 'tooltip-';
       var symbol = this.get('eventSymbol');
@@ -15,7 +18,20 @@ Checklist.TimelineEventListView = Ember.CollectionView.extend({
     contextBinding: 'content',
     template: Ember.Handlebars.compile(
       "<div class=\"circle {{unbound view.colour}} event\">{{unbound view.eventSymbol}}</div>" +
-      "<div id=\"popup{{unbound id}}\" class=\"lightbox\" style=\"display:none\">{{unbound notes}}</div>"
+
+      "<div class=\"lightbox\" style=\"display:none\" id=\"popup{{unbound id}}\">" +
+        "<div class=\"heading-holder\">" +
+          "<strong class=\"heading\">{{unbound view.eventDescription}} · <em class=\"date\">06/28/1979</em></strong>" +
+        "</div>" +
+        "<div class=\"text-holder\">" +
+          "<strong class=\"heading\">COUNTRY</strong>" +
+          "<p>Denmark</p>" +
+          "<div class=\"text-frame\">" +
+            "<strong class=\"heading\">NOTES</strong>" +
+            "<p>{{unbound notes}}</p>" +
+          "</div>" +
+        "</div>" +
+      "</div>"
     ),
     positionInPixels: function(){
       var total = this.get('parentView.parentView.totalWidthInPixels');
@@ -34,6 +50,17 @@ Checklist.TimelineEventListView = Ember.CollectionView.extend({
         return 'W';
       }
     }.property(),
+    eventDescription: function(){
+      if (this.get('content.change_type_name') == 'ADDITION'){
+        return 'ADDITION TO APPENDIX ' + this.get('appendix');
+      } else if (this.get('content.change_type_name') == 'DELETION'){
+        return 'DELETION FROM APPENDIX ' + this.get('appendix');
+      } else if (this.get('content.change_type_name') == 'RESERVATION'){
+        return 'RESERVATION';
+      } else if (this.get('content.change_type_name') == 'RESERVATION_WITHDRAWAL'){
+        return 'RESERVATION WITHDRAWAL';
+      }
+    }.property(),
     attributeBindings: ['style', 'title', 'href'],
     style: function() {
       return 'left:' + this.get('positionInPixels') + 'px';
@@ -44,7 +71,6 @@ Checklist.TimelineEventListView = Ember.CollectionView.extend({
       return (party !== null ? party + ' · ' + res : res);
     }.property(),
     href: function(){
-      //TODO
       return '#popup' + this.get('content.id');
     }.property(),
     didInsertElement: function(){
@@ -59,6 +85,24 @@ Checklist.TimelineEventListView = Ember.CollectionView.extend({
         this.get('eventSymbol') +'</div><div class="tooltip-text"></div>' +
         '<div class="tooltip-decor"></div></div>'
       });
+
+      this.$().fancybox({
+        padding: 10,
+        cyclic: false,
+        overlayShow: true,
+        overlayOpacity: 0.65,
+        overlayColor: '#000',
+        titlePosition: 'inside',
+        onComplete: function(box) {
+          if(link.attr('href').indexOf('#') === 0) {
+            jQuery('#fancybox-content').find('a.close').unbind('click.fb').bind('click.fb', function(e){
+              jQuery.fancybox.close();
+              e.preventDefault();
+            });
+          }
+        }
+      });
+
     }
   })
 });
