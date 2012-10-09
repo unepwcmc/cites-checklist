125343251dfdadbea20d99d32fd0f8f716a4782c
Adam Mulligan <adammulligan@gmail.com>
2012-10-09 14:30:24 +0100

    Improved the appearance of the saved search list

--- app/javascripts/views/saved_search_collection_view.js
+++ app/javascripts/views/saved_search_collection_view.js
@@ -4,21 +4,27 @@ Checklist.SavedSearchCollectionView = Ember.CollectionView.extend({
   itemViewClass: Ember.View.extend({
     contextBinding: 'content',
     templateName: 'saved_search_collection',
-    load: function(event) {
-      var selection = this.get('content').get('id');
-      var record    = Checklist.local_store.find(Checklist.SavedSearch, selection);
-      var filters   = JSON.parse(record.get('filters'));
 
-      var params = $.param(filters);
-
-      Checklist.get('router').transitionTo('search',{params: params});
-    },
     delete_search: function(event) {
       var id     = this.get('content').get('id');
       var search = Checklist.local_store.find(Checklist.SavedSearch, id);
 
       search.deleteRecord();
       Checklist.local_store.commit();
+    },
+
+    click: function(event) {
+      if ($(event.target).hasClass('delete')) return;
+
+      var router                 = Checklist.get('router');
+      var filtersController      = router.get('filtersController');
+      var taxonConceptController = router.get('taxonConceptController');
+
+      var selection = this.get('content').get('id');
+      var record    = Checklist.local_store.find(Checklist.SavedSearch, selection);
+      var filters   = JSON.parse(record.get('filters'));
+
+      taxonConceptController.refresh(filters);
     }
   })
 });
