unepwcmc/cites-checklist

commit 4f3458a6f120595dee69f0dac7ecbb52087751a1

Rearranged application view structure so that you don't have to re-render the filter controls every time you refresh the content

app/stylesheets/checklist.scss
@@ -36,9 +36,9 @@ body {
 .search:after,
 .c1:after,
 .content .block:after,
-#main:after,
+.main:after,
 .content .column-container:after,
-#header:after {
+.header:after {
   display:block;
   content:"";
   clear:both;
@@ -49,7 +49,7 @@ body {
   min-height:100%;
 }
 
-#header{
+.header{
   position:fixed;
   z-index:990;
   width:100%;
@@ -231,8 +231,8 @@ fieldset.saved-search {
   }
 }
 
-* +html #header .btn-holder{display:inline;}
-* +html #header .appendix-holder{display:inline;}
+* +html .header .btn-holder{display:inline;}
+* +html .header .appendix-holder{display:inline;}
 
 .drop {
   position:absolute;
@@ -650,7 +650,7 @@ fieldset.saved-search {
   margin:0 135px 0 0;
 }
 
-#main{
+.main{
   font-size:1.3em;
   line-height:1.23em;
   background: #fff;

app/javascripts/controllers/main_controller.js
@@ -1,0 +1,1 @@
+Checklist.MainController = Ember.Controller.extend({});

app/javascripts/controllers/taxon_concept_controller.js
@@ -1,6 +1,7 @@
 //TODO change this to extend ObjectController
 Checklist.TaxonConceptController = Ember.ArrayController.extend({
-  content: null,
+  content: Checklist.store.find(Checklist.Index, {}),
+
   contentIds: function(){
     return this.get('content').objectAt(0).get('contentIds');
   }.property('content'),

app/javascripts/routers/router.js
@@ -5,39 +5,43 @@ Checklist.Router = Ember.Router.extend({
     home: Ember.Route.extend({
       route: '/',
       connectOutlets: function(router, event) {
-        router.get('applicationController')
-        .connectOutlet({
-          viewClass: Checklist.MainView,
-          controller: router.get('taxonConceptController'),
-          context: Checklist.store.find(
-            Checklist.Index, router.get('filtersController').toParams()
-          )
-        });
+        router.get('applicationController').connectOutlet({outletName: 'header', viewClass: Checklist.MainHeaderView});
+        router.get('applicationController').connectOutlet({outletName: 'main', viewClass: Checklist.MainView});
       }
     }),
+
     search: Ember.Route.extend({
       route: '/search/:params',
       connectOutlets: function(router, event) {
-        params = Checklist.Helpers.deparam(event.params);
-        router.get('filtersController').fromParams(params);
+        var params = Checklist.Helpers.deparam(event.params);
 
-        router.get('applicationController')
-        .connectOutlet({
-          viewClass: Checklist.MainView,
-          controller: router.get('taxonConceptController'),
-          context: Checklist.store.find(
-            Checklist.Index, params
-          )
-        });
+        router.get('filtersController').fromParams(params);
+        router.get('taxonConceptController').refresh(params);
+
+        router.get('applicationController').connectOutlet({outletName: 'header', viewClass: Checklist.MainHeaderView});
+        router.get('applicationController').connectOutlet({outletName: 'main', viewClass: Checklist.MainView});
       }
     }),
+    search_without_render: Ember.Route.extend({
+      route: '/search/:params',
+      connectOutlets: function(router, event) {
+        var params = Checklist.Helpers.deparam(event.params);
+
+        router.get('filtersController').fromParams(params);
+        router.get('taxonConceptController').refresh(params);
+
+        router.get('applicationController').connectOutlet({outletName: 'main', viewClass: Checklist.MainView});
+      }
+    }),
+
     doAbout: function(router, event) {
       router.transitionTo('about');
     },
     about: Ember.Route.extend({
       route: '/about',
       connectOutlets: function(router, event) {
-        router.get('applicationController').connectOutlet({viewClass: Checklist.AboutView});
+        router.get('applicationController').connectOutlet({outletName: 'header', viewClass: Checklist.AboutHeaderView});
+        router.get('applicationController').connectOutlet({outletName: 'main', viewClass: Checklist.AboutView});
       }
     })
   })

app/javascripts/templates/about_header_view.handlebars
@@ -1,0 +1,19 @@
+<div class="wrap">
+  <div class="content">
+    <div class="c1">
+
+      <div class="logo-holder">
+        <h1 class="logo"><a href="/">Checklist of CITES Species</a></h1>
+        <a href="#" {{action "doAbout"}} class="top-links">About</a>
+      </div>
+
+      <div class="header-frame">
+        <h1>About CITES Checklist</h1>
+      </div>
+
+    </div>
+  </div>
+
+  <div class="aside">
+  </div>
+</div>

app/javascripts/templates/about_page.handlebars
@@ -1,82 +1,58 @@
-<header id="about-header">
-  <div class="wrap">
-    <div class="content">
-      <div class="c1">
+<div class="wrap">
+  <div class="content">
+    <div class="c1">
+      <div class="content-left">
+        <h2>CITES and the Checklist</h2>
+        <p>
+          CITES (the <strong>Convention on International Trade in
+          Endangered Species of Wild Fauna and Flora</strong>, also known as the
+          Washington Convention) is a multilateral treaty, the aim of
+          which is to ensure that international trade in specimens of
+          wild animals and plants does not threaten the survival of
+          the species in the wild, and it accords varying degrees of
+          protection to more than 33,000 species of animals and
+          plants. CITES works by subjecting international trade in
+          specimens of listed species to certain controls. These
+          require that all import, export, re-export and introduction
+          from the sea of species covered by the Convention has to be
+          authorized through a permitting system.
+        </p>
+        <p>
+          Roughly 5,000 species of animals and 29,000 species of
+          plants are protected by CITES against over-exploitation
+          through international trade. Each protected species or
+          population is included in one of three lists, called
+          Appendices. The Appendix that lists a
+          species or population reflects the extent of the threat to
+          it and the controls that apply to the trade.
+        </p>
 
-        <div class="logo-holder">
-          <h1 class="logo"><a href="/">Checklist of CITES Species</a></h1>
-          <a href="#" {{action "doAbout"}} class="top-links">About</a>
-        </div>
+        <h2>About the data</h2>
+        <p>
+          Praesent sit amet mi sit amet nisl pulvinar lacinia. Vestibulum euismod tristique neque, ac elementum nunc rutrum ac. Mauris iaculis gravida ipsum, in ultricies est pulvinar vitae. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc consequat, diam id dapibus tincidunt, turpis lectus porttitor eros, ut sollicitudin erat tellus ac ligula. Praesent volutpat quam sit amet leo dignissim viverra. Vivamus at tellus sed purus bibendum adipiscing ac congue erat. Proin eu libero metus. Nullam id dolor dictum neque rutrum accumsan. Fusce cursus elit at odio fringilla vulputate. Proin mauris elit, cursus a consequat ut, suscipit nec enim. Proin consectetur varius risus et molestie. Nulla convallis orci sit amet velit lacinia pellentesque.
+        </p>
 
-        <div class="header-frame">
-          <h1>About CITES Checklist</h1>
-        </div>
+        <h2>Our partners</h2>
+        <p>
+          Nulla blandit neque a odio fringilla non iaculis lacus congue. Vestibulum auctor turpis ac lectus auctor vulputate. Proin tempor dictum malesuada. Vivamus id nulla elit, in hendrerit felis. Cras massa justo, tincidunt at pharetra at, pharetra molestie enim. Fusce molestie ultricies quam vel dapibus. Suspendisse sollicitudin tortor vitae diam bibendum vestibulum. Pellentesque velit ligula, facilisis eget faucibus sit amet, pretium sed sem.
+        </p>
 
+        <h2>Contact</h2>
+        <p>
+          Nulla blandit neque a odio fringilla non iaculis lacus congue. Vestibulum auctor turpis ac lectus auctor vulputate. Proin tempor dictum malesuada. Vivamus id nulla elit, in hendrerit felis. Cras massa justo, tincidunt at pharetra at, pharetra molestie enim. Fusce molestie ultricies quam vel dapibus. Suspendisse sollicitudin tortor vitae diam bibendum vestibulum. Pellentesque velit ligula, facilisis eget faucibus sit amet, pretium sed sem.
+        </p>
+      </div>
+      <div class="menu-right">
+        <ul>
+          <li><a>CITES and the Checklist</a></li>
+          <li><a>About the data</a></li>
+          <li><a>Our partners</a></li>
+          <li><a>Contact</a></li>
+        </ul>
       </div>
     </div>
-
-    <div class="aside">
-    </div>
   </div>
-</header>
 
-<section id="about-main">
-  <div class="wrap">
-    <div class="content">
-      <div class="c1">
-        <div class="content-left">
-          <h2>CITES and the Checklist</h2>
-          <p>
-            CITES (the <strong>Convention on International Trade in
-            Endangered Species of Wild Fauna and Flora</strong>, also known as the
-            Washington Convention) is a multilateral treaty, the aim of
-            which is to ensure that international trade in specimens of
-            wild animals and plants does not threaten the survival of
-            the species in the wild, and it accords varying degrees of
-            protection to more than 33,000 species of animals and
-            plants. CITES works by subjecting international trade in
-            specimens of listed species to certain controls. These
-            require that all import, export, re-export and introduction
-            from the sea of species covered by the Convention has to be
-            authorized through a permitting system.
-          </p>
-          <p>
-            Roughly 5,000 species of animals and 29,000 species of
-            plants are protected by CITES against over-exploitation
-            through international trade. Each protected species or
-            population is included in one of three lists, called
-            Appendices. The Appendix that lists a
-            species or population reflects the extent of the threat to
-            it and the controls that apply to the trade.
-          </p>
-
-          <h2>About the data</h2>
-          <p>
-            Praesent sit amet mi sit amet nisl pulvinar lacinia. Vestibulum euismod tristique neque, ac elementum nunc rutrum ac. Mauris iaculis gravida ipsum, in ultricies est pulvinar vitae. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc consequat, diam id dapibus tincidunt, turpis lectus porttitor eros, ut sollicitudin erat tellus ac ligula. Praesent volutpat quam sit amet leo dignissim viverra. Vivamus at tellus sed purus bibendum adipiscing ac congue erat. Proin eu libero metus. Nullam id dolor dictum neque rutrum accumsan. Fusce cursus elit at odio fringilla vulputate. Proin mauris elit, cursus a consequat ut, suscipit nec enim. Proin consectetur varius risus et molestie. Nulla convallis orci sit amet velit lacinia pellentesque.
-          </p>
-
-          <h2>Our partners</h2>
-          <p>
-            Nulla blandit neque a odio fringilla non iaculis lacus congue. Vestibulum auctor turpis ac lectus auctor vulputate. Proin tempor dictum malesuada. Vivamus id nulla elit, in hendrerit felis. Cras massa justo, tincidunt at pharetra at, pharetra molestie enim. Fusce molestie ultricies quam vel dapibus. Suspendisse sollicitudin tortor vitae diam bibendum vestibulum. Pellentesque velit ligula, facilisis eget faucibus sit amet, pretium sed sem.
-          </p>
-
-          <h2>Contact</h2>
-          <p>
-            Nulla blandit neque a odio fringilla non iaculis lacus congue. Vestibulum auctor turpis ac lectus auctor vulputate. Proin tempor dictum malesuada. Vivamus id nulla elit, in hendrerit felis. Cras massa justo, tincidunt at pharetra at, pharetra molestie enim. Fusce molestie ultricies quam vel dapibus. Suspendisse sollicitudin tortor vitae diam bibendum vestibulum. Pellentesque velit ligula, facilisis eget faucibus sit amet, pretium sed sem.
-          </p>
-        </div>
-        <div class="menu-right">
-          <ul>
-            <li><a>CITES and the Checklist</a></li>
-            <li><a>About the data</a></li>
-            <li><a>Our partners</a></li>
-            <li><a>Contact</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-
-    <div class="aside">
-    </div>
+  <div class="aside">
   </div>
-</section>
+</div>

app/javascripts/templates/application_view.handlebars
@@ -1,5 +1,6 @@
 <div class="wrapper">
-  {{outlet}}
+  {{outlet header}}
+  {{outlet main}}
 </div>
 <footer id="footer">
   <div class="wrap">

app/javascripts/templates/main_header_view.handlebars
@@ -1,0 +1,43 @@
+<div class="wrap">
+  <div class="content">
+    <div class="c1">
+
+      <div class="logo-holder">
+        <h1 class="logo"><a href="/">{{tt title}}</a></h1>
+        <a href="#" {{action "doAbout"}} class="top-links">About</a>
+      </div>
+
+      <div class="header-frame">
+        <div class="column-holder">
+          <div class="column-frame">
+
+            <div class="column">
+              {{view Checklist.ChecklistForm
+                controllerBinding="controllers.taxonConceptController"
+                filtersControllerBinding="controllers.filtersController"}}
+            </div>
+
+            <div class="column">
+              {{view Checklist.AppendixFormView
+                contentBinding="controllers.filtersController.appendices"
+              }}
+            </div>
+
+            <div class="column style01">
+              {{view Checklist.LocationsView
+                regionsBinding="controllers.filtersController.regions"
+                countriesBinding="controllers.filtersController.countries"
+              }}
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  <div class="aside">
+    {{view Checklist.CustomSearch}}
+  </div>
+</div>

commit c8b53e995c9a11ab52a5475aa01f1180f5340b4c

Fixed autocomplete styling issues caused by previous commit (4f3458a6f120595dee69f0dac7ecbb52087751a1)

app/javascripts/vendor/bootstrap-typeahead.js
@@ -93,7 +93,7 @@
 
   , select: function () {
       // Do nothing when a header is clicked on
-      if (this.$menu.find('.active').hasClass('header')) { return; }
+      if (this.$menu.find('.active').hasClass('list-header')) { return; }
 
       var val = this.$menu.find('.active').attr('data-value')
       this.$element
@@ -238,7 +238,7 @@
       if (items.length === undefined) {
         var result = [];
         for (var cat in items) {
-          var i = $(that.options.item).text(cat.toString()).addClass('header');
+          var i = $(that.options.item).text(cat.toString()).addClass('list-header');
           result.push(i[0]);
 
           items[cat].forEach(function(item, i) {
@@ -281,7 +281,7 @@
       }
 
       // Don't allow headers to be selected
-      if (next.hasClass('header')) {
+      if (next.hasClass('list-header')) {
         next = next.next();
       }
 
@@ -296,7 +296,7 @@
         prev = this.$menu.find('li').last()
       }
 
-      if (prev.hasClass('header')) {
+      if (prev.hasClass('list-header')) {
         prev = prev.prev();
       }
 

app/stylesheets/checklist.scss
@@ -274,7 +274,7 @@ fieldset.saved-search {
     line-height:1.466em;
     color:#b6b6b6;
 
-    .header {
+    .list-header {
       display:block;
       padding:0 0 3px;
       text-transform:uppercase;