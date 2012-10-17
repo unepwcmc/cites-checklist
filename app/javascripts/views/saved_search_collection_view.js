9eb4ad9033bed07f26f71ba5007685d3f88db006
Adam Mulligan <adammulligan@gmail.com>
2012-10-17 10:56:09 +0100

    Fixed saved search transitions

--- app/javascripts/views/saved_search_collection_view.js
+++ app/javascripts/views/saved_search_collection_view.js
@@ -24,7 +24,7 @@ Checklist.SavedSearchCollectionView = Ember.CollectionView.extend({
       var record    = Checklist.local_store.find(Checklist.SavedSearch, selection);
       var filters   = JSON.parse(record.get('filters'));
 
-      taxonConceptController.refresh(filters);
+      router.transitionTo('search_without_render', {params: $.param(filters)});
     }
   })
 });
