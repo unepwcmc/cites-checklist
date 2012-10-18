230f8ded981e4a4dad9546dfc410bdaf708be657
Adam Mulligan <adammulligan@gmail.com>
2012-10-18 14:31:15 +0100

    Reverted CSS change to account for years

--- app/javascripts/views/taxon_concept_list_view.js
+++ app/javascripts/views/taxon_concept_list_view.js
@@ -146,17 +146,7 @@ Checklist.TaxonConceptListView = Ember.CollectionView.extend({
           this.$().fadeIn();
         });
       }
-
-    },
-
-    becameVisible: function() {
-      // Ensure centre vertical alignment for appendix icon and countries
-      // list in the taxa list
-      $('.column-container').sameHeight({
-        elements: '.column-area',
-        flexible: false
-      });
-    },
+    }
   })
 });
 
