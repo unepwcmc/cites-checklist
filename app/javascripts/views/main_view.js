0a185dc3ed157d536daaf8827a7930bdfe7b5b59
Adam Mulligan <adammulligan@gmail.com>
2012-10-31 09:17:07 +0000

    Fixed colorbox close button and made "explore" button close window

--- app/javascripts/views/main_view.js
+++ app/javascripts/views/main_view.js
@@ -35,12 +35,25 @@ Checklist.MainView = Ember.View.extend({
         opacity: 0.8,
         fixed: true,
         open: true,
+        onOpen: function(event) {
+          // A truly awful hack to ensure that the close button is
+          // aligned properly. Without modifying colorbox, there's no
+          // way to make the CSS for the close button for this box
+          // distinct from the css for the close button for other boxes
+          // of different widths.
+          $('#cboxClose').css({right: '30px'});
+        },
         onClosed: function(event) {
           $.cookie("Checklist.CONFIG.visited", true);
+          $('#cboxClose').css({right: '10px'});
         }
       };
 
       $('#landing-btn').colorbox(landing_colorbox_config);
     }
+  },
+
+  closeLandingBox: function(event) {
+    $.colorbox.close();
   }
 });
