2a779c00a9e07fc376e2d599e4ef4e6bf93ee43c
strtwtsn <stuart.watson@unep-wcmc.org>
2013-07-11 15:51:00 +0100

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,8 +7,7 @@ require 'capistrano/ext/multistage'
 
 set :generate_webserver_config, false
 
-set(:pub_key) { Capistrano::CLI.ui.ask ("Enter Name of Public key: ") }
-ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", pub_key)]
+ssh_options[:forward_agent] = true
 
  
 # Target directory for the application on the web and app servers.
