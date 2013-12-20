af3653ad6b4163381a0509fa15ec17a18236663e
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2013-12-20 12:18:41 +0000

    [Fixes #62556276] IE10 issue with territories missing from dropdown

--- app/javascripts/models/country.js
+++ app/javascripts/models/country.js
@@ -5,5 +5,9 @@ Checklist.Country = DS.Model.extend({
 });
 
 Checklist.Country.reopenClass({
-  collectionUrl: 'geo_entities?geo_entity_types[]=country&geo_entity_types[]=territory'
+  // IE10 for some reason aappends a questionmark to this url
+  // consequently, territories would not get fetched for location dropdown
+  // this is an ugly workaround to a problem that seems to be too ember-data
+  // specific
+  collectionUrl: 'geo_entities?geo_entity_types[]=country&geo_entity_types[]=territory&1'
 });
