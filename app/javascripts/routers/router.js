29772788a6569f6f93e1a6592c6801738258dc8f
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-03-20 09:51:57 +0000

    [#37746913] another attempt at fixing this amazing bug

--- app/javascripts/routers/router.js
+++ app/javascripts/routers/router.js
@@ -30,51 +30,50 @@ Checklist.Router = Ember.Router.extend({
       index: Ember.Route.extend({
         route: '/',
         connectOutlets: function(router, event) {
-          router.get('filtersController').set(
-            'countriesContent',
-            Checklist.store.findQuery(
-              Checklist.Country, {geo_entity_types_set: 2, locale: Em.I18n.currentLocale}
-            )
-          );
-          router.get('filtersController').set(
-            'regionsContent',
-            Checklist.store.findQuery(
-              Checklist.Country, {geo_entity_types_set: 1, locale: Em.I18n.currentLocale}
-            )
-          );
-          var params = router.get('filtersController').toParams();
-          router.get('taxonConceptController').refresh(params);
+          Checklist.LocationsLoader.load().then(
+            function(locations){
+              router.get('filtersController').set(
+                'countriesContent', locations.countries
+              );
+              router.get('filtersController').set(
+                'regionsContent', locations.regions
+              );
+              var params = router.get('filtersController').toParams();
+              router.get('taxonConceptController').refresh(params);
 
-          router.get('applicationController').connectOutlet({outletName: 'header', viewClass: Checklist.MainHeaderView});
-          router.get('applicationController').connectOutlet({outletName: 'main', viewClass: Checklist.MainView});
+              router.get('applicationController').connectOutlet({outletName: 'header', viewClass: Checklist.MainHeaderView});
+              router.get('applicationController').connectOutlet({outletName: 'main', viewClass: Checklist.MainView});
+            }, function(error){
+              console.log(error);
+            }
+          );
         }
       }),
 
       search: Ember.Route.extend({
         route: '/search/:params',
         connectOutlets: function(router, event) {
-          router.get('filtersController').set(
-            'countriesContent',
-            Checklist.store.findQuery(
-              Checklist.Country, {geo_entity_types_set: 2, locale: Em.I18n.currentLocale}
-            )
+          Checklist.LocationsLoader.load().then(
+            function(locations){
+              router.get('filtersController').set(
+                'countriesContent', locations.countries
+              );
+              router.get('filtersController').set(
+                'regionsContent', locations.regions
+              );
+              var params = Checklist.Helpers.deparam(event.params);
+
+              router.get('filtersController').fromParams(params);
+              router.get('taxonConceptController').refresh(params);
+
+              if (event.redraw === undefined || event.redraw) {
+                router.get('applicationController').connectOutlet({outletName: 'header', viewClass: Checklist.MainHeaderView});
+              }
+              router.get('applicationController').connectOutlet({outletName: 'main', viewClass: Checklist.MainView});
+            }, function(error){
+              console.log(error);
+            }
           );
-          router.get('filtersController').set(
-            'regionsContent',
-            Checklist.store.findQuery(
-              Checklist.Country, {geo_entity_types_set: 1, locale: Em.I18n.currentLocale}
-            )
-          );
-          var params = Checklist.Helpers.deparam(event.params);
-
-          router.get('filtersController').fromParams(params);
-          router.get('taxonConceptController').refresh(params);
-
-          if (event.redraw === undefined || event.redraw) {
-            router.get('applicationController').connectOutlet({outletName: 'header', viewClass: Checklist.MainHeaderView});
-          }
-
-          router.get('applicationController').connectOutlet({outletName: 'main', viewClass: Checklist.MainView});
         }
       }),
 
