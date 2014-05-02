67b3a8ac9c8c695399b8db344cc815de0c190aa1
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-05-02 11:37:46 +0100

    add translation links with colorbox-aware handlers to splash screen

--- app/javascripts/views/main_view.js
+++ app/javascripts/views/main_view.js
@@ -4,39 +4,50 @@ Checklist.MainView = Ember.View.extend({
 
   classNames: ['main'],
 
+  landingColorboxConfig: {
+    inline: true,
+    transition: 'fade',
+    close: 'x',
+    opacity: 0.8,
+    fixed: true,
+    maxWidth: 675,
+    innerWidth: 675,
+    open: true,
+    className: 'landing',
+    onClosed: function(event) {
+      $.cookie("Checklist.CONFIG.visited", true);
+    }
+  },
+
   didInsertElement: function() {
     var target = document.getElementById('loading');
     var spinner = new Spinner(Checklist.CONFIG.spinner).spin(target);
 
     if (!$.cookie("Checklist.CONFIG.visited")) {
-      var landing_colorbox_config = {
-          inline: true,
-          transition: 'fade',
-          close: 'x',
-          opacity: 0.8,
-          fixed: true,
-          maxWidth: 675,
-          innerWidth: 675,
-          open: true,
-          onOpen: function(event) {
-            // A truly awful hack to ensure that the close button is
-            // aligned properly. Without modifying colorbox, there's no
-            // way to make the CSS for the close button for this box
-            // distinct from the css for the close button for other boxes
-            // of different widths.
-            $('#cboxClose').css({right: '30px'});
-          },
-          onClosed: function(event) {
-            $.cookie("Checklist.CONFIG.visited", true);
-            $('#cboxClose').css({right: '10px'});
-          }
-        };
-
-      $('#landing-btn').colorbox(landing_colorbox_config);
+      $('#landing-btn').colorbox(this.get('landingColorboxConfig'));
     }
   },
 
   closeLandingBox: function(event) {
     $.colorbox.close();
+  },
+
+  doLocaleEN: function(event) {
+    $.colorbox.remove();
+    Checklist.router.transitionTo('home.index', {locale: 'en'});
+    $('#landing-btn').colorbox(this.get('landingColorboxConfig'));
+  },
+
+  doLocaleES: function(event) {
+    $.colorbox.remove();
+    Checklist.router.transitionTo('home.index', {locale: 'es'});
+    $('#landing-btn').colorbox(this.get('landingColorboxConfig'));
+  },
+
+  doLocaleFR: function(event) {
+    $.colorbox.remove();
+    Checklist.router.transitionTo('home.index', {locale: 'fr'});
+    $('#landing-btn').colorbox(this.get('landingColorboxConfig'));
   }
+
 });
