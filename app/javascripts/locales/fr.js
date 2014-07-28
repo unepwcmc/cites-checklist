a6ae88e0ac41f9da35acd9d37918faa25ccb3eab
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-07-28 15:17:47 +0100

    [#75587748] fixed remaining French & Spanish Checklist titles

--- app/javascripts/locales/fr.js
+++ app/javascripts/locales/fr.js
@@ -99,6 +99,6 @@ Em.I18n.locales.fr = {
   'reports.latest.title': 'Votre rapport est en cours de production',
 
   'splash.p1': 'La CITES (Convention sur le commerce international des espèces de faune et de flore sauvages menacées d\'extinction) est un accord international entre Etats qui vise à s\'assurer que le commerce international de spécimens d\'animaux ou de plantes sauvages ne menace pas la survie de ces derniers',
-  'splash.p2': 'La liste de contrôle CITES permet d\'explorer plus de 35 000 espèces animales ou végétales et de vérifier leur degré de protection',
+  'splash.p2': 'La Liste des espèces CITES permet d\'explorer plus de 35 000 espèces animales ou végétales et de vérifier leur degré de protection',
   'splash.button': 'Commencer l\'exploration'
 };
