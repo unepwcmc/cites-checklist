6d18acf9201c286042679694bf4533d5f06c7da7
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-07-29 17:11:22 +0100

    rewritten the observer logic

--- app/javascripts/controllers/taxon_concept_controller.js
+++ app/javascripts/controllers/taxon_concept_controller.js
@@ -1,27 +1,28 @@
 Checklist.TaxonConceptController = Ember.ArrayController.extend({
-  content: [],
-
-  taxaCount: 0,
-
-  contentIds: function(){
-    return this.get('content').objectAt(0).get('contentIds');
-  }.property('content'),
-
-  contentDidChange: function(){
-    if (this.get('content.isLoaded')){
-      this.set('taxaCount', this.get('content').objectAt(0).get('total_cnt'));
-      Checklist.store.find(
-        Checklist.TimelinesForTaxonConcept, {
-          taxon_concept_ids: this.get('contentIds'), locale: Em.I18n.currentLocale
-        }
-      );
-    }
-  }.observes('content.@each.isLoaded','contentIds'),
-
-  refresh: function(params) {
-    this.set('content', Checklist.store.find(
+  refresh: function(params){
+    var that = this;
+    var promise = new RSVP.Promise();
+    var res = Checklist.store.find(
       Checklist.Index,
       $.extend({locale: Em.I18n.currentLocale}, params)
-    ));
+    );
+    res.addObserver(
+      '@each.didLoad',
+      function(){
+        Ember.run.once(
+          this,
+          function() { promise.resolve(res); }
+        );
+       }
+    );
+    promise.then(function(taxon_concepts_index_ary){
+      that.set('content', taxon_concepts_index_ary);
+      var contentIds = taxon_concepts_index_ary.objectAt(0).get('contentIds');
+      Checklist.store.find(
+        Checklist.TimelinesForTaxonConcept, {
+          taxon_concept_ids: contentIds, locale: Em.I18n.currentLocale
+        }
+      );
+    });
   }
 });
