049290d48417efa8c750f428361364cbadd2e5ba
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-07-29 14:10:20 +0100

    removed unnecessary property setting

--- app/javascripts/controllers/taxon_concept_controller.js
+++ app/javascripts/controllers/taxon_concept_controller.js
@@ -6,16 +6,14 @@ Checklist.TaxonConceptController = Ember.ArrayController.extend({
   contentIds: function(){
     return this.get('content').objectAt(0).get('contentIds');
   }.property('content'),
-  contentHistory: null,
+
   contentDidChange: function(){
     if (this.get('content.isLoaded')){
       this.set('taxaCount', this.get('content').objectAt(0).get('total_cnt'));
-      this.set('contentHistory',
-        Checklist.store.find(
-          Checklist.TimelinesForTaxonConcept, {
-            taxon_concept_ids: this.get('contentIds'), locale: Em.I18n.currentLocale
-          }
-        )
+      Checklist.store.find(
+        Checklist.TimelinesForTaxonConcept, {
+          taxon_concept_ids: this.get('contentIds'), locale: Em.I18n.currentLocale
+        }
       );
     }
   }.observes('content.@each.isLoaded','contentIds'),
