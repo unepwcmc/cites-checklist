59d2964e7e4d87f55a0c50f6e4610f38cea17093
Stuart Watson <stuart.watson@unep-wcmc.org>
2015-07-28 12:32:03 +0100

    Removed incorrect shared files

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,7 +37,7 @@ before "deploy:symlink:shared", "rsync:sync"
 
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/mailer_config.yml config/secrets.yml config.js}
+#set :linked_files, %w{config/database.yml config/mailer_config.yml config/secrets.yml config.js}
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
