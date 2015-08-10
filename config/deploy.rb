eed576e42a53792cbe570521c315f9ef1bb8869b
strtwtsn <stuart.watson@unep-wcmc.org>
2015-08-10 11:44:31 +0100

    Updated config.js path

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :ssh_options, {
 before "deploy:symlink:shared", "symlink:config"
 
 # Default value for :linked_files is []
-#set :linked_files, %w{app/javascripts/config.js}
+set :linked_files, %w{app/javascripts/config.js}
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
