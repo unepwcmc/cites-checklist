918337e3024e39ef3945c5570c4a4f2ace4cab94
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2016-06-30 11:55:12 +0100

    fix for country names not getting translated

--- app/javascripts/controllers/geo_entities_controller.js
+++ app/javascripts/controllers/geo_entities_controller.js
@@ -1,14 +1,20 @@
 Checklist.GeoEntitiesController = Ember.ArrayController.extend({
   regions: null,
   countries: null,
+  needsReload: false,
 
   load: function(){
-    if (!this.get('regions.isLoaded')){
-      this.set('regions', Checklist.GeoEntity.find({geo_entity_types_set: '1'}));
+    if (!this.get('regions.isLoaded') || this.get('needsReload')){
+      this.set('regions', Checklist.GeoEntity.find({
+        geo_entity_types_set: '1', locale: Em.I18n.currentLocale
+      }));
     }
-    if (!this.get('countries.isLoaded')){
-      this.set('countries', Checklist.GeoEntity.find({geo_entity_types_set: '2'}));
+    if (!this.get('countries.isLoaded') || this.get('needsReload')){
+      this.set('countries', Checklist.GeoEntity.find({
+        geo_entity_types_set: '2', locale: Em.I18n.currentLocale
+      }));
     }
+    this.set('needsReload', false)
   }
 
 });
