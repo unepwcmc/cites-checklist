bb9899cda5f89c5e705931a53944a64f894a93f7
Will Kocur <will.kocur@unep-wcmc.org>
2020-01-21 16:20:22 +0000

    add styling and test using data/actions from view

--- app/javascripts/views/id_materials_view.js
+++ app/javascripts/views/id_materials_view.js
@@ -1,3 +1,9 @@
 Checklist.IdMaterialsView = Ember.View.extend({
-  templateName: 'id_materials_view'
+  templateName: 'id_materials_view',
+  taxonConceptId: 0,
+  idMaterials: [1,2,3,4,5,6],
+
+  downloadIdManualEntries: function () {
+    console.log(this.get('taxonConceptId'))
+  }
 });
