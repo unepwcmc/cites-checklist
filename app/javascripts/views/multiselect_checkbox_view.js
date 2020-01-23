173c343b31412aa4ce83551eb145dc9e8d3c086c
Will Kocur <will.kocur@unep-wcmc.org>
2020-01-23 16:27:36 +0000

    add select all button

--- app/javascripts/views/multiselect_checkbox_view.js
+++ app/javascripts/views/multiselect_checkbox_view.js
@@ -1,3 +1,5 @@
+const ALL_ID = '__all__'
+
 Checklist.MultiselectCheckboxView = Ember.View.extend({
   options: [
     {label: 'Vol 1', id:'1'},
@@ -11,24 +13,67 @@ Checklist.MultiselectCheckboxView = Ember.View.extend({
   tagName: 'fieldset',
   class: '',
   templateName: 'multiselect_checkbox_view',
+  selectAllLabel: 'All volumes',
+  areAllSelected: false,
 
   optionsForView: function () {
-    return this.get('options').map(function (option) {
+    const options = this.get('options').slice(0)
+
+    options.unshift({
+      id: ALL_ID,
+      label: this.get('selectAllLabel')
+    })
+
+    return options.map(function (option) {
       return {
         id: option.id,
         label: option.label,
-        checkboxId: this.get('id') + '-checkbox-' + option.id
+        checkboxId: this.getCheckboxId(option.id)
       }
     }.bind(this))
-  }.property('id', 'options'),
+  }.property('id', 'options', 'selectAllLabel'),
+
+  getCheckboxId: function (optionId) {
+    return this.get('id') + '-checkbox-' + optionId
+  },
 
   select: function (id) {
     this.selected.push(id)
+
+    if (this.selected.length === this.options.length) {
+      this.areAllSelected = true
+      this.checkAll()
+    }
   },
 
   deselect: function (id) {
     this.selected = this.selected.filter(function (currentId) {
       return currentId !== id
+    })
+
+    if (this.selected.length === this.options.length - 1) {
+      this.areAllSelected = false
+      this.uncheckSelectAllCheckbox()
+    }
+  },
+
+  checkAll: function () {
+    this.setCheckboxes(true)
+  },
+
+  uncheckAll: function () {
+    this.setCheckboxes(false)
+  },
+
+  uncheckSelectAllCheckbox: function () {
+    this
+      .$('#' + this.getCheckboxId(ALL_ID))
+      .prop('checked', false)
+  },
+
+  setCheckboxes: function (areChecked) {
+    this.$('input[type=checkbox]').each(function () {
+      $(this).prop('checked', areChecked)
     })
   },
 
@@ -37,15 +82,26 @@ Checklist.MultiselectCheckboxView = Ember.View.extend({
   },
 
   toggleCheckbox: function (event) {
-    console.log(this.selected)
-
     const id = event.target.value
 
-    if (this.isSelected(id)) {
+    if (id === ALL_ID) {
+      this.toggleAllCheckboxes()
+    } else if (this.isSelected(id)) {
       this.deselect(id)
     } else {
       this.select(id)
     }
-    console.log('after', this.selected)
   },
+
+  toggleAllCheckboxes () {
+    this.areAllSelected = !this.areAllSelected
+
+    if (this.areAllSelected) {
+      this.selected = this.options.map(function (opt) { return opt.id })
+      this.checkAll()
+    } else {
+      this.selected = []
+      this.uncheckAll()
+    }
+  }
 })
