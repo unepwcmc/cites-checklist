075a6eda63aabe6aab8c60990a757cf9a3d11387
Will Kocur <will.kocur@unep-wcmc.org>
2020-06-01 14:12:33 +0100

    fix about jump links

--- app/javascripts/views/about_view.js
+++ app/javascripts/views/about_view.js
@@ -2,5 +2,15 @@ Checklist.AboutView = Ember.View.extend({
   tagName: 'section',
   templateName: 'about_page',
 
-  classNames: ['about-main']
+  classNames: ['about-main'],
+
+  didInsertElement: function () {
+    this.$('.scroll-to').each(function (_, linkEl) {
+      $(linkEl).on("click", function () { 
+        const anchorId = $(this).data('anchor-id')
+
+        $('#' + anchorId)[0].scrollIntoView()
+      })
+    })
+  }
 });
