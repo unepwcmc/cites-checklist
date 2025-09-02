2ac5753d1d164dba0db09b8c5d347fdf0594374f
Daniel Perrett <daniel.perrett@unep-wcmc.org>
2025-09-02 14:16:00 +0100

    feat: show distribution tags

--- app/javascripts/models/cites_population.js
+++ app/javascripts/models/cites_population.js
@@ -1,4 +1,15 @@
 Checklist.CitesPopulation = Ember.Object.extend({
   name: null,
-  species_listing_name: null
+  geo_entity_id: null,
+  tag_names: null,
+  species_listing_name: null,
+  maybe_tag_names: function(){
+    var tagNames = this.get('tag_names');
+
+    return (
+      tagNames.length
+      ? ' (' + tagNames.join(', ') + ')'
+      : ''
+    )
+  }.property(),
 });
